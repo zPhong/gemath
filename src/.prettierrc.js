4c089adb0c9236a0aca12ccc022f0a595f4b7334
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-07-13 13:34:50 +0700

    :heavy_plus_sign: Add prettier, bootstrap

---
+++ src/.prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tslintIntegration: false,
+  eslintIntegration: false,
+  printWidth: 120,
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  useTabs: false,
+  jsxBracketSameLine: true
+};
