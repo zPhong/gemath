f4423e01d49a1d6609485ccd631ed7c924b1c612
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-07-14 15:00:54 +0700

    :art: Update values

---
+++ src/utils/values.js
@@ -0,0 +1,7 @@
+export const INFINITY = 'vô cực';
+export const IMPOSSIBLE = 'vô nghiệm';
+export const TOO_SHORT = 'quá ngắn';
+export const NOT_ENOUGH_SET = 'không đủ phương trình tạo thành hệ';
+export const NOT_BE_IN_LINE = 'điểm không thuộc đường';
+export const MIN_RANDOM_NUMBER = -10;
+export const MAX_RANDOM_NUMBER = 10;
