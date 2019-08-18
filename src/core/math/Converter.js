4b71b2b41f8c903ea87b3e2f60beb409afb40248
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-08-18 10:58:00 +0700

    Add convert circle from standard to general type

--- src/core/math/Converter.js
+++ src/core/math/Converter.js
@@ -1,4 +1,8 @@
-import type { EquationType, LineType } from '../../utils/types';
+import type {
+  CoordinateType,
+  EquationType,
+  LineType,
+} from '../../utils/types';
 
 export function convertLinearToEquation(l: EquationType): EquationType {
   return {
@@ -6,14 +10,21 @@ export function convertLinearToEquation(l: EquationType): EquationType {
     b: 0,
     c: l.c,
     d: l.d,
-    e: l.e
+    e: l.e,
   };
 }
 
 export function convertEquationToLineType(line: EquationType): LineType {
   return {
-    a: -line.c / (line.d === 0 ? 1 : line.d),
-    b: -line.e / (line.d === 0 ? 1 : line.d)
+    a: -line.c /
+      (line.d === 0 ?
+        1 :
+        line.d),
+
+    b: -line.e /
+      (line.d === 0 ?
+        1 :
+        line.d),
   };
 }
 
@@ -23,7 +34,32 @@ export function convertLineTypeToEquation(line: LineType): EquationType {
     b: 0,
     c: -line.a,
     d: 1,
-    e: -line.b
+    e: -line.b,
   };
+}
+
+export function convertCircleEquation(centerPoint: CoordinateType, radius: number): EquationType {
+  let results = undefined;
+
+  if (radius < 0) {
+    results = 'Radius must be a positive number';
+  }
+
+  const cX = centerPoint.x;
+  const cY = centerPoint.y;
+
+  const cXSquare = cX * cX;
+  const cYSquare = cY * cY;
+  const rSquare = radius * radius;
+
+  results = {
+    a: 1,
+    b: 1,
+    c: -2 * cX,
+    d: -2 * cY,
+    e: cXSquare + cYSquare - rSquare,
+  };
+
+  return results;
 }
 
