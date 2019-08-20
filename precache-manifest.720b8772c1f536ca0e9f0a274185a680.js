27b0fcf3d348ba46f50b7ae15374e5576b0ef692
Lục Trường Phong <luctruongphong13@gmail.com>
2019-08-20 17:45:26 +0700

    Updates

---
+++ precache-manifest.720b8772c1f536ca0e9f0a274185a680.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fcc3eb54850d4bd610b5d7d04ddc0e",
+    "url": "/gemath/index.html"
+  },
+  {
+    "revision": "9ef6968eca022e0a0931",
+    "url": "/gemath/static/css/main.1e11cea4.chunk.css"
+  },
+  {
+    "revision": "4d9e71b27c273ebc837c",
+    "url": "/gemath/static/js/2.0074ec14.chunk.js"
+  },
+  {
+    "revision": "9ef6968eca022e0a0931",
+    "url": "/gemath/static/js/main.d1ae8f29.chunk.js"
+  },
+  {
+    "revision": "62b015dc5458ece259f9",
+    "url": "/gemath/static/js/runtime~main.2912cca0.js"
+  },
+  {
+    "revision": "f7f81caab38d1bc5e29abb436ff9f9e5",
+    "url": "/gemath/static/media/HATINH.f7f81caa.TTF"
+  },
+  {
+    "revision": "35321a782a3cbb7f9cc0353450a7145e",
+    "url": "/gemath/static/media/HelveticaNeue-Light.35321a78.otf"
+  },
+  {
+    "revision": "18a81d43652c0f91aa924ed6f33d5989",
+    "url": "/gemath/static/media/Nunito-Light.18a81d43.ttf"
+  }
+]);
