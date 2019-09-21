6c672648283c5ac9ccada915f68d80804472dc4c
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-09-21 12:33:31 +0700

    [Core] :bug: Update check init params

--- src/core/config/GLog.js
+++ src/core/config/GLog.js
@@ -2,13 +2,13 @@ import GConfig from './GConfig';
 
 export default class GLog {
   static logInfo(moduleName, ...args) {
-    if (!GConfig.isOffLog) {
+    if (!GConfig.isOffLog && moduleName && moduleName.constructor) {
       console.info(`[${moduleName.constructor.name}]`, ...args);
     }
   }
 
   static logMsg(moduleName, msg, offThis = false) {
-    if (!GConfig.isOffLog && !offThis) {
+    if (!GConfig.isOffLog && moduleName && moduleName.constructor && !offThis) {
       console.log(`[${moduleName.constructor.name}]`, msg);
     }
   }
