604d7c8b9a3d97d19e37789d2a3cf097734e41df
Le Vo Gia Khang <levogiakhang@gmail.com>
2019-09-21 13:31:02 +0700

    [Core] Add checker isDev

--- src/core/config/GLog.js
+++ src/core/config/GLog.js
@@ -2,19 +2,19 @@ import GConfig from './GConfig';
 
 export default class GLog {
   static logInfo(moduleName, ...args) {
-    if (!GConfig.isOffLog && moduleName && moduleName.constructor) {
+    if (!GConfig.isOffLog && GConfig.isDev && moduleName && moduleName.constructor) {
       console.info(`[${moduleName.constructor.name}]`, ...args);
     }
   }
 
   static logMsg(moduleName, msg, offThis = false) {
-    if (!GConfig.isOffLog && moduleName && moduleName.constructor && !offThis) {
+    if (!GConfig.isOffLog && GConfig.isDev && moduleName && moduleName.constructor && !offThis) {
       console.log(`[${moduleName.constructor.name}]`, msg);
     }
   }
 
   static logMsgWithLineBreaks(moduleName, ...msgs) {
-    if (!GConfig.isOffLog && moduleName && moduleName.constructor) {
+    if (!GConfig.isOffLog && GConfig.isDev && moduleName && moduleName.constructor) {
       let msg = undefined;
       if (Array.isArray(msgs)) {
         msg = msgs.join('\n');
@@ -24,7 +24,7 @@ export default class GLog {
   }
 
   static logError(moduleName, error, offThis = false) {
-    if (!GConfig.isOffLog && moduleName && moduleName.constructor && !offThis) {
+    if (!GConfig.isOffLog && GConfig.isDev && moduleName && moduleName.constructor && !offThis) {
       console.error(`[${moduleName.constructor.name}]`, error);
     }
   }
